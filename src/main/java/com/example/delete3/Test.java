5044e8cb40ed66ec7a5e5ddab165c8581f2cac81
gorno <xalivan@ukr.net>
2021-12-05 17:50:08 +0200

    0002

--- src/main/java/com/example/delete3/Test.java
+++ src/main/java/com/example/delete3/Test.java
@@ -3,5 +3,7 @@ package com.example.delete3;
 public class Test {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
     }
 }
