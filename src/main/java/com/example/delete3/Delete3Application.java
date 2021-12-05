baed9e9d473b609a1284fa1b39db241157d56d95
gorno <xalivan@ukr.net>
2021-12-05 17:12:05 +0200

    1 m

---
+++ src/main/java/com/example/delete3/Delete3Application.java
@@ -0,0 +1,13 @@
+package com.example.delete3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Delete3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Delete3Application.class, args);
+    }
+
+}
