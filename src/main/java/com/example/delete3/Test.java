ad2172fac6ada8ed30c93513eafa31e3bcb218dd
gorno <xalivan@ukr.net>
2021-12-05 17:21:40 +0200

    2 b

--- src/main/java/com/example/delete3/Test.java
+++ src/main/java/com/example/delete3/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("mine 1");
         System.out.println("b 1");
+        System.out.println("b 2");
     }
 }
